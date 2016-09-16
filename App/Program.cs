1c5b1501650af9ac8c719006db50e3c80846611b
Andreas Johansson <ahjflex@gmail.com>
2016-09-16 10:55:12 +0200

    methods/classes defined

--- App/Program.cs
+++ App/Program.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App {
-    class Program {
-        static void Main(string[] args) {
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool CheckAge(Person p|)
+        {
+            throw new NotImplementedException();
+        }
+
+        static int[] DoubleAll(int[] xs)
+        {
+             throw new NotImplementedException();
+        }
+
+        static string FizzBuzz(int x)
+        {
+            throw new NotImplementedException();
         }
     }
 }
